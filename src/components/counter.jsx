295d49216b8374d8d0291e622194711fe8f7814c
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-07-11 20:16:19 +0300

    code from lesson all added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React,{useState} from "react"
 
 const Counter = (props) => {
     
-    const [value, setValue] = useState(props.value)
+    // const [value, setValue] = useState(props.value)
+    const {value} = props
 
     const formatValue = () => {
         return value === 0 ? 'empty' : value
@@ -17,12 +18,14 @@ const Counter = (props) => {
     }
 
     const handleIncrement = () => {
-        setValue((prevState) => prevState + 1)
+        console.log('handle Increment');
+        // setValue((prevState) => prevState + 1)
     }
 
     
     const handleDecrement = () => {
-        setValue((prevState) => prevState - 1)
+        console.log('handle Decrement');
+        // setValue((prevState) => prevState - 1)
     }
 
     return (
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -3,18 +3,36 @@ import Counter from './counter'
 
 const CountersList = () => {
 
-    const [counters, setCounters] = useState([
+    const initialState = [
         {id: 0, value: 0, name:"Ненужная вещь"},
         {id: 1, value: 4, name:"Ложка"},
         {id: 2, value: 0, name:"Вилка"},
         {id: 3, value: 0, name:"Тарелка"},
         {id: 4, value: 0, name:"Набор минималиста"}
-    ])
+    ]
+
+    const [counters, setCounters] = useState(initialState)
 
     const handleDelete = (id) => {
         console.log('handleDelete', id);
         const newCounters = counters.filter(c => c.id !== id)
         setCounters((preState) => preState = newCounters)
+    }
+
+    const handleReset = () => {
+        setCounters(initialState)
+    }
+
+    const handleUpdate = () => {
+
+        const updatedState = [
+            {id: 0, value: 1, name:"Ненужная вещь"},
+            {id: 1, value: 2, name:"Ложка"},
+            {id: 2, value: 3, name:"Вилка"},
+            {id: 3, value: 4, name:"Тарелка"},
+            {id: 4, value: 0, name:"Набор минималиста"}
+        ]
+        setCounters(updatedState)
     }
 
     return <>
@@ -24,7 +42,8 @@ const CountersList = () => {
                 {...count}
                 onDelete = {handleDelete}/>
         )}
-
+        <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>Сброс</button>
+        <button className="btn btn-primary btn-sm m-2" onClick={handleUpdate}>Обновить состояние</button>
     </>
 
 }
