b5dfe78445a2be98d878a3384c9b2c1ffa7feaf9
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-07-11 18:17:02 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './app';
+
+const container = document.querySelector('#root')
+const root = createRoot(container)
+
+root.render(<App/>)
