3df26a2e2726b9b7e0979483a567feaf04c49e61
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-07-12 15:48:15 +0300

    homeWork 1 ready

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React,{useState} from "react"
 const Counter = (props) => {
     
     // const [value, setValue] = useState(props.value)
-    const {value} = props
+    const {value, id, onIncrement, onDecrement} = props
 
     const formatValue = () => {
         return value === 0 ? 'empty' : value
@@ -17,25 +17,25 @@ const Counter = (props) => {
 
     }
 
-    const handleIncrement = () => {
-        console.log('handle Increment');
-        // setValue((prevState) => prevState + 1)
-    }
+    // const handleIncrement = () => {
+    //     console.log('handle Increment');
+    //     // setValue((prevState) => prevState + 1)
+    // }
 
     
-    const handleDecrement = () => {
-        console.log('handle Decrement');
-        // setValue((prevState) => prevState - 1)
-    }
+    // const handleDecrement = () => {
+    //     console.log('handle Decrement');
+    //     // setValue((prevState) => prevState - 1)
+    // }
 
     return (
     <div>
         <span>{props.name}</span>
         <span className={getClasses()}>{formatValue()}</span>
-        <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
-        <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>-</button>
+        <button className="btn btn-primary btn-sm m-2" onClick={() => onIncrement(id)}>+</button>
+        <button className="btn btn-primary btn-sm m-2" onClick={() => onDecrement(id)}>-</button>
         <button className="btn-danger btn-sm m-2"
-            onClick={() => props.onDelete(props.id)}
+            onClick={() => props.onDelete(id)}
         >
             Delete
         </button>
